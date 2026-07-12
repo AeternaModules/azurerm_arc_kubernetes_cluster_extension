f481a1583143069f4ab8c311fadbae06642d1392
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:57 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,49 +1,45 @@
-output "arc_kubernetes_cluster_extensions" {
-  description = "All arc_kubernetes_cluster_extension resources"
-  value       = azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions
-}
 output "arc_kubernetes_cluster_extensions_cluster_id" {
-  description = "List of cluster_id values across all arc_kubernetes_cluster_extensions"
-  value       = [for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : v.cluster_id]
+  description = "Map of cluster_id values across all arc_kubernetes_cluster_extensions, keyed the same as var.arc_kubernetes_cluster_extensions"
+  value       = { for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : k => v.cluster_id }
 }
 output "arc_kubernetes_cluster_extensions_configuration_protected_settings" {
-  description = "List of configuration_protected_settings values across all arc_kubernetes_cluster_extensions"
-  value       = [for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : v.configuration_protected_settings]
+  description = "Map of configuration_protected_settings values across all arc_kubernetes_cluster_extensions, keyed the same as var.arc_kubernetes_cluster_extensions"
+  value       = { for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : k => v.configuration_protected_settings }
 }
 output "arc_kubernetes_cluster_extensions_configuration_settings" {
-  description = "List of configuration_settings values across all arc_kubernetes_cluster_extensions"
-  value       = [for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : v.configuration_settings]
+  description = "Map of configuration_settings values across all arc_kubernetes_cluster_extensions, keyed the same as var.arc_kubernetes_cluster_extensions"
+  value       = { for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : k => v.configuration_settings }
 }
 output "arc_kubernetes_cluster_extensions_current_version" {
-  description = "List of current_version values across all arc_kubernetes_cluster_extensions"
-  value       = [for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : v.current_version]
+  description = "Map of current_version values across all arc_kubernetes_cluster_extensions, keyed the same as var.arc_kubernetes_cluster_extensions"
+  value       = { for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : k => v.current_version }
 }
 output "arc_kubernetes_cluster_extensions_extension_type" {
-  description = "List of extension_type values across all arc_kubernetes_cluster_extensions"
-  value       = [for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : v.extension_type]
+  description = "Map of extension_type values across all arc_kubernetes_cluster_extensions, keyed the same as var.arc_kubernetes_cluster_extensions"
+  value       = { for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : k => v.extension_type }
 }
 output "arc_kubernetes_cluster_extensions_identity" {
-  description = "List of identity values across all arc_kubernetes_cluster_extensions"
-  value       = [for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : v.identity]
+  description = "Map of identity values across all arc_kubernetes_cluster_extensions, keyed the same as var.arc_kubernetes_cluster_extensions"
+  value       = { for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : k => v.identity }
 }
 output "arc_kubernetes_cluster_extensions_name" {
-  description = "List of name values across all arc_kubernetes_cluster_extensions"
-  value       = [for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : v.name]
+  description = "Map of name values across all arc_kubernetes_cluster_extensions, keyed the same as var.arc_kubernetes_cluster_extensions"
+  value       = { for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : k => v.name }
 }
 output "arc_kubernetes_cluster_extensions_release_namespace" {
-  description = "List of release_namespace values across all arc_kubernetes_cluster_extensions"
-  value       = [for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : v.release_namespace]
+  description = "Map of release_namespace values across all arc_kubernetes_cluster_extensions, keyed the same as var.arc_kubernetes_cluster_extensions"
+  value       = { for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : k => v.release_namespace }
 }
 output "arc_kubernetes_cluster_extensions_release_train" {
-  description = "List of release_train values across all arc_kubernetes_cluster_extensions"
-  value       = [for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : v.release_train]
+  description = "Map of release_train values across all arc_kubernetes_cluster_extensions, keyed the same as var.arc_kubernetes_cluster_extensions"
+  value       = { for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : k => v.release_train }
 }
 output "arc_kubernetes_cluster_extensions_target_namespace" {
-  description = "List of target_namespace values across all arc_kubernetes_cluster_extensions"
-  value       = [for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : v.target_namespace]
+  description = "Map of target_namespace values across all arc_kubernetes_cluster_extensions, keyed the same as var.arc_kubernetes_cluster_extensions"
+  value       = { for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : k => v.target_namespace }
 }
 output "arc_kubernetes_cluster_extensions_version" {
-  description = "List of version values across all arc_kubernetes_cluster_extensions"
-  value       = [for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : v.version]
+  description = "Map of version values across all arc_kubernetes_cluster_extensions, keyed the same as var.arc_kubernetes_cluster_extensions"
+  value       = { for k, v in azurerm_arc_kubernetes_cluster_extension.arc_kubernetes_cluster_extensions : k => v.version }
 }
 
